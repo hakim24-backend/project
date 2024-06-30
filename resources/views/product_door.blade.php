fe4e9ff262a952074052a783661fc66f23960df7
hakim24-backend <mtaufiqalhakim24@gmail.com>
2024-07-01 00:22:36 +0700

    update door product less slider door

--- resources/views/product_door.blade.php
+++ resources/views/product_door.blade.php
@@ -1,234 +1,200 @@
-<?php 
-    use App\Models\Description;
-?>
 <!DOCTYPE html>
-<html lang="ru" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="shortcut icon" type="x-icon" href="{{asset('/upload/img/SG_верт_оригин.png')}}">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>{{$product->name}}</title>
-    <link
-      rel="stylesheet"
-      type="text/css"
-      media="screen"
-      href="{{asset('/css/normalize.css')}}"
-    />
+<html lang="en">
 
-    <link
-      rel="stylesheet"
-      type="text/css"
-      media="screen"
-      href="{{asset('/css/bootstrap.min.css')}}"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-      integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title></title>
 
-    <link
-      href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;400;700&family=Ubuntu:wght@300;400;700&display=swap"
-      rel="stylesheet"
-    />
+    {{-- css header and footer --}}
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/slick.css')}}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/slick-theme.css')}}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/responsive.css')}}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/search.css')}}">
 
-    
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
-
-
-    <link
-      rel="stylesheet"
-      type="text/css"
-      media="screen"
-      href="{{asset('/css/slick.css')}}"
-    />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      media="screen"
-      href="{{asset('/css/slick-theme.css')}}"
-    />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      media="screen"
-      href="{{asset('/css/layout_grid_door.css')}}"
-    />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      media="screen"
-      href="{{asset('/css/responsive.css')}}"
-    />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      media="screen"
-      href="{{asset('/css/product_detail_door.css')}}"
-    />
-    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('/css/search.css')}}">
-    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('/css/zoom_img.css')}}"/>
+    <link rel="stylesheet" href="{{asset('door/css/magnify.css')}}" />
+    <link rel="stylesheet" href="{{asset('door/css/zoomple.css')}}" />
+    <link rel="stylesheet" href="{{asset('door/css/style.css?v=1234')}}" />
     <style>
+        .section-padding {
+            padding-top: 50px;
+            padding-bottom: 210px;
+        }
 
-      .secondImageNew .simg {
-        width: 415px;
-        height: 300px;
-        margin-bottom: 80px;
-        margin-top: -95px;
-        zoom: 100%;
-        margin-left: 40px;
-        padding-top: 15%;
-      } 
 
-      .secondImageNew img:hover {
-        transform: none ;
-        box-shadow: none;
-      }
 
-      #magnifying_area {
-        /* max-width: 500px;
-        height: auto; */
-        position: relative;
-        margin: 10px auto;
-      }
-    
-      #magnifying_img {
-        width: 100%;
-        height: auto;
-      }
-    
-      #magnifying_area:hover,
-      #magnifying_area:active {
-        cursor: zoom-in;
-        display: block;
-      }
-    
-      #magnifying_area:hover #magnified_img,
-      #magnifying_area:active #magnified_img {
-        opacity: 1;
-      }
-    
-      #magnified_img {
-        width: 340px;
-        height: 340px;
-        background: url('{{asset('upload/product/'.$product->filename)}}') no-repeat #FFF;
-        box-shadow: 0 5px 10px -2px rgba(0, 0, 0, 0.3);
-        pointer-events: none;
-        position: absolute;
-        opacity: 0;
-        border: 4px solid whitesmoke;
-        z-index: 99;
-        border-radius: 100%;
-        display: block;
-        transition: opacity .2s;
-      }
     </style>
+</head>
 
-  </head>
-
-  <body data-spay="scroll" data-target=".nav" data-offset="100">
+<body>
 
     {{-- Header --}}
     @include('components-frontend.header')
 
-    <!-- Layout grid -->
-    <div class="gallerys">
+    <div class="container-door">
+        <div class="innerContainer">
+            <!-- <div class="row addressBar">
+			<p>Главная > межкомнатные двери > Cерия HISPANIA > МОДЕЛЬ III (3) / ПАНЕЛЬ </p>
+		</div> -->
 
-    </div>
-        
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5">
-                    <div class="project-info-box mt-0">
-                        <br><br><br><br><br>
-                        <h5>{{$product->name}}</h5>
-                        <!-- <p class="mb-0">Vivamus pellentesque, felis in aliquam ullamcorper, lorem tortor porttitor erat, hendrerit porta nunc tellus eu lectus. Ut vel imperdiet est. Pellentesque condimentum, dui et blandit laoreet, quam nisi tincidunt tortor.</p> -->
-                    </div><!-- / project-info-box -->
-        
-                    <div class="project-info-box">
-                      <h2 class="mb-0">спецификация</h2> <br>
-                      @foreach ($description as $item)
-                          <p><b>{{$item->name}} :</b> {{$item->value}}</p>
-                      @endforeach
+            <div class="breadcrumbs1">
+                <br><br>
+                <div itemscope="" itemtype="http://schema.org/BreadcrumbList" class="container">
+                    <span itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                        <a class="first" itemprop="item" href="{{route('frontend.index')}}" title="Главная"><span itemprop="name">Главная</span>
+                            <meta itemprop="position" content="0"> >
+                        </a>
+                    </span>
+
+                    <span itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                        <a class="first" itemprop="item" href="{{route('frontend.category', $product->collection->category->name)}}"
+                            title="{{$product->collection->category->name}}"><span itemprop="name">межкомнатные двери</span>
+                            <meta itemprop="position" content="1"> >
+                        </a>
+                    </span>
+
+                    <span itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                        <a class="first" itemprop="item" href="{{route('frontend.collection', $product->collection->id)}}" title="{{$product->collection->name}}"><span
+                                itemprop="name">{{$product->collection->name}}</span>
+                            <meta itemprop="position" content="2"> >
+                        </a>
+                    </span>
+
+                    <span itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem"><span
+                            itemprop="name">{{$product->name}}</span>
+                        <meta itemprop="position" content="4">
+                    </span>
+                </div>
+            </div><br>
+            <div class="productSec">
+                <h1 class="text-center">{{$product->name}}</h1>
+                <div class="productBox">
+                    <div class="productSpecs">
+                        <h3>спецификация</h3>
+                        @foreach ($description as $item)
+                        <p><b>{{$item->name}} :</b> {{$item->value}}</p>
+                        @endforeach
+
+
+                        <!-- Thumbnail images -->
+                        <p class="noborder">другой декор:</p>
+                        <div class="thumbnailsRow">
+                            <div class="column">
+
+                                <img class="demo cursor" src="{{asset('door/images/slider/brown.png')}}"
+                                    onclick="openModal('{{asset('door/images/slider/brown.png')}}')" alt="The Woods" />
+
+                                <p>Door</p>
+
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="{{asset('door/images/slider/dark-brown.png')}}"
+                                    onclick="openModal('{{asset('door/images/slider/dark-brown.png')}}')"
+                                    alt="Cinque Terre">
+                                <p>Door</p>
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="{{asset('door/images/slider/light-brown.png')}}"
+                                    onclick="openModal('{{asset('door/images/slider/light-brown.png')}}')"
+                                    alt="Mountains and fjords">
+                                <p>Door</p>
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="{{asset('door/images/slider/off-white.png')}}"
+                                    onclick="openModal('{{asset('door/images/slider/off-white.png')}}')"
+                                    alt="Northern Lights">
+                                <p>Door</p>
+                            </div>
+                            <div id="myModal" class="modal">
+                                <span class="close" onclick="closeModal()">&times;</span>
+                                <a href="" class="zoomHover2" id="zoomContainer">
+                                    <img class="modalZoom" id="modalImage">
+                                </a>
+                            </div>
+                        </div>
+                        <br>
                     </div>
-                    <!-- / project-info-box -->
-        
-                    <div class="project-info-box mt-0 mb-0">
-                        <p class="mb-0">
-                            <!-- <span class="fw-bold mr-10 va-middle hide-mobile">Share:</span>
-                            <a href="#x" class="btn btn-xs btn-linkedin btn-circle btn-icon mr-5 mb-0"><i class="fab fa-linkedin-in"></i></a> -->
-                        </p>
+                    <div class="productImg">
+                        <a href="{{asset('upload/product/'.$product->filename)}}" class="zoomHover">
+                            <img src="{{asset('upload/product/'.$product->filename)}}" class="zoom">
+                        </a>
                     </div>
-                    <!-- / project-info-box -->
-                </div><!-- / column -->
-        
-        
-                <div class="col-md-4">
-                  <div id="magnifying_area">
-                    <img id="magnifying_img" src="{{asset('upload/product/'.$product->filename)}}" alt="project-image">
-                    <div id="magnified_img"></div>
-                  </div>
-                    <div class="project-info-box">
-                        <!-- <p><b>Категории:</b> Столешницы Серия Color</p>
-                        <p><b>Серия:</b> Color</p> -->
-                    </div><!-- / project-info-box -->
-                </div><!-- / column -->
+                </div>
             </div>
         </div>
-          
+    </div>
 
-         
-        
-          
-    <!-- End of Layout grid -->
-    
     {{-- Footer --}}
     @include('components-frontend.footer')
 
-    <script src="{{asset('/js/zoom_img.js')}}"></script>
-    <script src="{{asset('/js/jquery.min.js')}}"></script>
-    <script src="{{asset('/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('/js/slick.min.js')}}"></script>
-    <script src="{{asset('/js/main.js')}}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
-      let zoomer = function () {
-          document.querySelector('#magnifying_area').addEventListener('mousemove', function (e) {
+        function modalMagnify() {
+            $(document).ready(function () {
+                $('.modalZoom').magnify();
+            });
+        }
 
-              let original = document.querySelector('#magnifying_img'),
-                magnified = document.querySelector('#magnified_img'),
-                style = magnified.style,
-                rect = this.getBoundingClientRect(),
-                x = e.pageX - rect.left - window.scrollX,
-                y = e.pageY - rect.top - window.scrollY,
-                imgWidth = original.offsetWidth,
-                imgHeight = original.offsetHeight,
-                xperc = ((x / imgWidth) * 100),
-                yperc = ((y / imgHeight) * 100);
-              //lets user scroll past right edge of image
-              if (x > (.01 * imgWidth)) {
-                xperc += (.15 * xperc);
-              };
-
-              //lets user scroll past bottom edge of image
-              if (y >= (.01 * imgHeight)) {
-                yperc += (.15 * yperc);
-              };
-
-              style.backgroundPositionX = (xperc - 9) + '%';
-              style.backgroundPositionY = (yperc - 9) + '%';
-
-              style.left = (x - 180) + 'px';
-              style.top = (y - 180) + 'px';
-
-            }, false);
-        }();
     </script>
-  </body>
+    <script>
+        // Function to open modal with selected image
+        function openModal(imageSrc) {
+            var modal = document.getElementById("myModal");
+            var modalImg = document.getElementById("modalImage");
+            modal.style.display = "block";
+            modalImg.src = imageSrc;
+            zoomContainer.href = imageSrc;
+            modalMagnify();
+        }
+
+        // Function to close modal
+        function closeModal() {
+            var modal = document.getElementById("myModal");
+            modal.style.display = "none";
+        }
+
+        // Close modal if user clicks outside the image
+        window.onclick = function (event) {
+            var modal = document.getElementById("myModal");
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
+    </script>
+    <script src="{{asset('door/js/jquery.magnify.js')}}"></script>
+    <script src="{{asset('door/js/jquery.magnify-mobile.js')}}"></script>
+    <script>
+        $(document).ready(function () {
+            $('.zoom').magnify();
+
+            $('#zoomple_image_overlay').mouseenter(function () {
+                $(".modalZoom").addClass("hoverEffect");
+            });
+            $('#zoomple_image_overlay').mouseleave(function () {
+                $(".modalZoom").removeClass("hoverEffect");
+            });
+        });
+
+    </script>
+    <script>
+
+    </script>
+
+</body>
+
 </html>
