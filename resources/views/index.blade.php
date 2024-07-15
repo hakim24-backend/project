51e39d6633ce4d0811d6de4ab52c3b3426078e32
hakim24-backend <mtaufiqalhakim24@gmail.com>
2024-07-16 00:21:51 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
   <link rel="shortcut icon" type="x-icon" href="{{asset('/upload/img/SG_верт_оригин.png')}}">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <title>Промышленная группа Союз</title>
   <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/normalize.css')}}">
 
